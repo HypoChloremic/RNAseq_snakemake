e4c686b7dd7d950c309d07da59f06eb0756f8072
Ali Rassolie <ali.rassolie@gmail.com>
2017-07-17 18:25:24 +0200

    Add files via upload

--- file_name_producer.py
+++ file_name_producer.py
@@ -1,5 +1,7 @@
 # Snakemake pipe for RNAseq
 # (c) 2017 Ali Rassolie
+__version__ = "0.1.0 VAGRANT"
+
 
 print(
 """
@@ -15,85 +17,45 @@ To terminate run, press ctrl+Z
 """)
 input()
 
-
 from subprocess import call
 import os, re
 
-class Constants:
-  pass
+class FileExpander:
+  def __init__(self):
 
+# This is the config file, necessary effective functioning.
+# Please do note that we are doing this relative to the Snakefile folder, and not the scripts
+# folder. Some strange python thing.
+    configfile = "scripts/config.yaml"
+    clean = lambda i: [ re.findall(r"(.*)_\d\.fastq\.gz", z)[0] for z in i if re.findall(r"(.*)_\d\.fastq\.gz", z)]
 
-configfile = "config.yaml"
+# Here we open the configfile to access the parent directory; we should maybe use JSON formatting,
+# snakemake might not like it alas.
+    try:
+      PARENT_DIR, SYMLINKED_DIR = self.opener(configfile)
+    except FileNotFoundError as e:
+      PARENT_DIR, SYMLINKED_DIR = self.opener("config.yaml")
+# Now we are defining the global variable FULL_PATHS
+# The variable is important to produce the file-names
+# the rules require
 
-
-
-with open(configfile, "r") as file:
-  text_data = file.read().replace('"','').replace("'","").split("\n")
-  text_data = [ i.split(": ") for i in text_data ]
-  input (text_data)
-  PARENT_DIR = txt_convert("PARENT_DIR", text_data)
-  INDEX = txt_convert("INDEX", text_data)
-
-
-  PARENT_DIR = config["PARENT_DIR"]
-  INDEX = config["INDEX"]
-  
-
-def txt_convert(i, txt):
-  pass
-
-def isgz(i):
-  bool = re.search(r"\w\.gz", i)
-  try:
-    assert bool
-    return True
-  except AssertionError:
-    return False
-
-
-
-def file_expander(n=3):
-  pass
-
-
+# Please do make sure that the input PARENT_DIR is a list.
+    self.FULL_PATHS = file_expander([PARENT_DIR])
+    self.SYMLINKED_PATHS = symlink_creator(origin=self.FULL_PATHS, target=SYMLINKED_DIR)
+    print(self.SYMLINKED_PATHS)
 
 # This lambda func will replace remove the suffix of the files that we find
-clean = lambda i: [ re.findall(r"(\w+)_\d\.fastq\.gz", z) for z in i if z]
+    self.FULL_PATHS = clean(self.FULL_PATHS)
+    self.SYMLINKED_PATHS = clean(self.SYMLINKED_PATHS)
+    print("symlinked")
+    print("\n".join(self.SYMLINKED_PATHS))
+    input()
 
-sample = PARENT_DIR
-dir_list = os.listdir(sample)
 
-print(dir_list)
-
-# This is so as to present the folders in the current directory
-first_dir_child = [ i for i in dir_list if os.path.isdir(f"{sample}/{i}") ]
-
-# Now we would like to explore every single folder that is in first_dir_child
-full_paths = []
-filenames = []
-
-print(first_dir_child)
-
-for folder in first_dir_child:
-  if not folder == ".snakemake":
-    temp_path = f"{sample}/{folder}"
-    temp_list = os.listdir(temp_path)
-
-   # "We have an index 0 here, which may be problematic if there are several folders within the nest"
-    temp_path = f"{sample}/{folder}/{temp_list[0]}"
-    print(temp_path)
-    temp_list2 = os.listdir(f"{temp_path}")
-    act_paths = [ f"{temp_path}/{i}" for i in temp_list2 if isgz(i) ]
-    temp_act_paths = clean(act_paths)
-    print(f"temp {temp_act_paths}")
-
-    for index in range(len(temp_act_paths)):
-      try:
-        full_paths.append(f"{temp_path}/{temp_act_paths[index][0]}")
-        break
-      except IndexError as e:
-        pass
-  else:
-    pass
-
- 
+  def opener(self, i):
+    with open(i, "r") as file:
+      text_data = file.read().replace('"','').replace("'","").split("\n")
+      text_data = { i.split(": ")[0]:i.split(": ")[1] for i in text_data if i}
+      PARENT_DIR = text_data["PARENT_DIR"]
+      SYMLINKED_DIR = text_data["SYMLINKED_DIR"]
+      return PARENT_DIR, SYMLINKED_DIR
